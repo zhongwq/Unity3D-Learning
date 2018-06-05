b6cd2fa7b966d7be39d45302186727b8274a02da
zhongwq <1316628630@qq.com>
2018-06-05 16:23:39 +0800

    14: Upload hw 8

---
+++ Homework8/Assets/Toggle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Toggle : MonoBehaviour {
+	private Button yourButton;  
+	public Text text;  
+	private Animator animator;
+
+	// Use this for initialization  
+	void Start() {  
+		Button btn = this.gameObject.GetComponent<Button>();  
+		animator = text.GetComponent<Animator> ();
+		btn.onClick.AddListener(ClickBtn);  
+	}  
+
+	void ClickBtn() {  
+		if (animator.GetBool ("State")) {
+			animator.SetTrigger ("ToggleIn");
+			animator.SetBool ("State", false);
+		} else {
+			animator.SetTrigger ("ToggleOut");
+			animator.SetBool ("State", true);
+		}
+	}
+}
